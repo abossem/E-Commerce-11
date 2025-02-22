c36217199bd46332e4d5ae6115381396a2d9f6f4
Abdulrhman Fahmy <abdelrhmanfahmy69@gmail.com>
2025-02-22 15:10:51 +0200

    nav & header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Footer from "./Components/Footer/Footer";
+import NavBar from "./Components/NavBar/NavBar";
+
 function App() {
   return (
-    <div className="text-xl underline text-blue-600 font-bold font-inter">
-      Hello World From Tailwindcss!
-    </div>
+    <>
+      <NavBar />
+      <Footer />
+    </>
   );
 }
 
