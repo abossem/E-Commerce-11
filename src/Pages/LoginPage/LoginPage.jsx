b9ba1294f8f9b294fda4f3ab9bf4b5f1422bb181
abdulllah ali <engineerabdallah67@gmail.com>
2025-02-24 11:38:29 +0200

    Create Auth Pages

---
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,80 @@
+import { IoMdArrowDropright } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center flex-col items-center min-h-screen container">
+      <div>
+        <div className="bg-white p-8 rounded border border-gray-100 max-w-[500px]">
+          <div className="flex justify-center mb-4">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
+              alt="Amazon Logo"
+              className="h-6"
+            />
+            <span className="text-sm text-gray-500">.in</span>
+          </div>
+
+          <h3 className="font-semibold mb-4 text-3xl">Sign in</h3>
+
+          <form>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Email or mobile phone number
+            </label>
+            <input type="text" className="w-full border-1 rounded p-2 mb-4 " />
+          </form>
+
+          <button className="w-full cursor-pointer bg-yellow-500 text-white py-2 rounded">
+            Continue
+          </button>
+
+          <p className="text-xs text-gray-500 mt-2">
+            By continuing, you agree to Amazon&apos;s{" "}
+            <a href="#" className="text-blue">
+              Conditions of Use
+            </a>{" "}
+            and{" "}
+            <a href="#" className="text-blue">
+              Privacy Notice
+            </a>
+            .
+          </p>
+
+          <div className="mt-4 text-sm flex  items-center gap-1">
+            <IoMdArrowDropright />
+            <Link to="/" className="text-blue">
+              Need help?
+            </Link>
+          </div>
+
+          <div className="mt-6 pt-4 border-t border-gray-100">
+            <h4 className="font-semibold text-xl">Buying for work?</h4>
+            <Link to={`/`} className="text-blue">
+              Shop on Amazon Business
+            </Link>
+          </div>
+        </div>
+        <div>
+          <div className="relative flex items-center justify-center mt-6">
+            <div className="absolute left-0 w-1/3 h-[1px] bg-gray-100 bottom-1/2 translate-y-1/2"></div>
+            <span className="bg-white px-2 text-sm text-gray-500">
+              New to Amazon?
+            </span>
+            <div className="absolute right-0 w-1/3 h-[1px] bg-gray-100 bottom-1/2 translate-y-1/2"></div>
+          </div>
+
+          <Link
+            to={`/register`}
+            className="w-full block text-center p-4 transition-all  mt-2 cursor-pointer border border-gray-400 py-2 rounded hover:bg-gray-200">
+            Create your Amazon account
+          </Link>
+        </div>
+      </div>
+      <div className="w-full flex justify-center border-t pt-3 border-gray-100 items-center mt-8">
+        <span className="text-sm text-gray-500 py-3">
+          &copy; 1996-2024, Amazon.com, Inc. or its affiliates
+        </span>
+      </div>
+    </div>
+  );
+}
---
+++ src/Pages/LoginPage/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,94 @@
+import { IoMdArrowDropright } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+export default function RegisterPage() {
+  return (
+    <div className="flex justify-center flex-col items-center min-h-screen container">
+      <div className="w-full max-w-md bg-white p-6 rounded-md border border-gray-100">
+        <div className="flex justify-center mb-4">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
+            alt="Amazon Logo"
+            className="h-6"
+          />
+          <span className="text-sm text-gray-500">.in</span>
+        </div>
+
+        <h2 className="text-xl font-semibold mb-4">Create Account</h2>
+
+        <form>
+          <div className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Your name
+              </label>
+              <input
+                type="text"
+                className="w-full p-2 border border-gray-300 rounded-md"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Mobile number
+              </label>
+              <input
+                type="text"
+                className="w-full p-2 border border-gray-300 rounded-md"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                className="w-full p-2 border border-gray-300 rounded-md"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-yellow-400 text-black py-2 rounded-md font-medium">
+              Verify mobile number
+            </button>
+          </div>
+        </form>
+
+        <div className="mt-4 text-sm">
+          <p className="font-medium text-xl">Buying for work?</p>
+          <Link to={``} className="text-blue">
+            Create a free business account
+          </Link>
+        </div>
+
+        <div className="mt-4 text-sm">
+          <p className="flex items-center gap-1">
+            <span className="text-[19px]"> Already have an account? </span>
+            <Link to={`/login`} className="text-blue flex items-center gap-1">
+              Sign in
+              <IoMdArrowDropright />
+            </Link>
+          </p>
+          <p className="text-2xs mt-2 ">
+            By creating an account or logging in, you agree to Amazon&apos;s{" "}
+            <a href="#" className="text-blue">
+              Conditions of Use
+            </a>{" "}
+            and{" "}
+            <Link to="" className="text-blue">
+              Privacy Notice
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+      <div className="w-full flex justify-center border-t pt-3 border-gray-100 items-center mt-8">
+        <span className="text-sm text-gray-500 py-3">
+          &copy; 1996-2024, Amazon.com, Inc. or its affiliates
+        </span>
+      </div>
+    </div>
+  );
+}
