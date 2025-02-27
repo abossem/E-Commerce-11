27678d73e49ea4014e018a97f5bc5aa0623d6967
Abdulrhman Fahmy <abdelrhmanfahmy69@gmail.com>
2025-02-27 03:09:32 +0200

    userContext

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,68 @@
+import { useFormik } from "formik";
 import { IoMdArrowDropright } from "react-icons/io";
+import { useUserContext } from "../../context/User.context";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { object, string } from "yup";
+import axios from "axios";
 
 
 export default function LoginPage() {
+
+  let navigate = useNavigate()
+  const {setToken} = useUserContext();
+  const [accountError, setAccountError] = useState(null);
+
+const passwordRegex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+
+  let schema = object({
+    email: string().required("Email is required").email("Invalid Email"),
+    password: string()
+      .required("Password is required")
+      .matches(
+        passwordRegex,
+        "Minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character"
+      ),
+  });
+
+  async function onSubmit(values) {
+    try {
+     const options = {
+       url:'https://e-commerce-11-api.vercel.app/api/api/login',
+       method:'POST',
+       data: values
+     }
+     let {data} = await axios.request(options)
+     if (data.status == 'success') {
+       setToken(data.data.token)
+       console.log(data.data.user);
+       setTimeout(() => {
+         navigate('/')
+       }, 2000);
+              localStorage.setItem('token',data.data.token)
+       console.log(data);
+     }
+      
+    } catch (error) {
+     console.log(error);
+     setAccountError(error.response.data.message);
+ 
+     
+    }
+   }
+
+  let formik = useFormik({
+      initialValues:{
+      email: "",
+      password: ""
+      },
+      validationSchema:schema,
+      onSubmit,
+    })
+
+
+
   return (
     <div className="flex justify-center flex-col items-center min-h-screen w-full  mt-6">
       <div>
@@ -17,20 +78,31 @@ export default function LoginPage() {
 
           <h3 className="font-semibold mb-4 text-3xl">Sign in</h3>
 
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <label className="block text-sm font-medium text-gray-700 mb-1">
               Email
             </label>
-            <input type="text" className="w-full border-1 rounded p-2 mb-4 " />
+            <input type="email" name="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}  className="w-full border-1 rounded p-2 mb-4 " />
+            {formik.errors.email && formik.touched.email && (
+            <p className="text-red-400 mt-1 text-sm">*{formik.errors.email}</p>
+          )}
+          {accountError && (
+            <p className="text-red-400 mt-1 text-sm">*{accountError}</p>
+          )}
             <label className="block text-sm font-medium text-gray-700 mb-1">
              Password
             </label>
-            <input type="text" className="w-full border-1 rounded p-2 mb-4 " />
+            <input type="password" name="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} className="w-full border-1 rounded p-2 mb-4 " />
+            {formik.errors.password && formik.touched.password && (
+            <p className="text-red-400 mt-1 text-sm">*{formik.errors.password}</p>
+          )}
+
+<button type="submit" className="w-full cursor-pointer bg-yellow-500 text-white py-2 rounded">
+            Log in
+          </button>
           </form>
 
-          <button className="w-full cursor-pointer bg-yellow-500 text-white py-2 rounded">
-            Continue
-          </button>
+          
 
           <p className="text-xs text-gray-500 mt-2">
             By continuing, you agree to Amazon&apos;s{" "}
---
+++ src/context/User.context.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const UserContext = createContext()
+
+
+
+export default function UserProvider ({children}) {
+
+    const [token, setToken] = useState(localStorage.getItem('token'))
+    const [userInfo,setUserInfo] = useState(null)
+
+
+    async function userData() {
+       try {
+        const options = {
+            url:'https://e-commerce-11-api.vercel.app/api/api/user',
+            method:'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+              },
+        }
+        let {data} = await axios.request(options)
+        if (data.status == 'success') {
+            setUserInfo(data.data)
+        }
+        
+       } catch (error) {
+        console.log(error);
+        
+       }
+    }
+
+    async function logOut() {
+        try {
+            const options = {
+                url:'https://e-commerce-11-api.vercel.app/api/api/logout',
+                method:'POST',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                  },
+            }
+            let {data} = await axios.request(options)
+            if (data.status == 'success') {
+                setToken(null)
+                localStorage.removeItem('token')
+            }
+            
+            
+           } catch (error) {
+            console.log(error);
+            
+           }
+    }
+
+    useEffect(()=>{
+        userData()
+    },[token])
+    
+return <UserContext.Provider value={{token,setToken,userInfo,setUserInfo,logOut}}>{children}</UserContext.Provider>
+}
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+  return context;
+};
